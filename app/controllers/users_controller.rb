5d2f564482947d3b1e1627edd657e746776ca425
Katsumasa-Toda <katsumasatoda7@gmail.com>
2021-02-27 08:12:11 +0000

    Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :ensure_correct_user, only:[:edit]
+  # before_action :ensure_correct_user, only:[:edit]
 
   def show
     @user = User.find(params[:id])
@@ -11,11 +11,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
   end
 
   def update
